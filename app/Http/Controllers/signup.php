9d6b2c46f6f065a7625afc90ce3058c08bb9d944
husseininjass <hussein.husam7@gmail.com>
2023-09-10 11:44:36 +0300

    signup validation

--- app/Http/Controllers/signup.php
+++ app/Http/Controllers/signup.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\signups;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class signup extends Controller
 {
@@ -16,9 +17,9 @@ class signup extends Controller
         $data_input = $request->validate([
             'fname' => 'required|min:3|max:255',
             'lname' => 'required|min:3|max:255',
-            'password' => 'required|min:5|max:255',
             'email' => 'required|email',
-            'confirm_password' => 'required|max:255',
+            'password' => 'required_with:confirm_password|same:confirm_password|max:255|min:5',
+            'confirm_password' => 'required',
             'phone' => 'required|numeric',
         ]);
         $user_data = new signups; 
@@ -26,7 +27,7 @@ class signup extends Controller
         $user_data->Lname = $data_input['lname'];
         $user_data->email = $data_input['email'];
         $user_data->phone = $data_input['phone'];
-        $user_data->password = bcrypt($data_input['password']);
+        $user_data->password = Crypt::encrypt($data_input['password']);        
         $user_data->save();
         return redirect()->route('home');
     }
